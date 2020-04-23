75dce6274eaae10b1a7f0124bce0788ad5a222f3
nijashansen <nijas.w.hansen@gmail.com>
2020-04-23 13:46:33 +0200

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDQa8SPKBDaDQcQTky0SPH21F4AVOGiPik',
+    authDomain: 'feast-782f3.firebaseapp.com',
+    databaseURL: 'https://feast-782f3.firebaseio.com',
+    projectId: 'feast-782f3',
+    storageBucket: 'feast-782f3.appspot.com',
+    messagingSenderId: '554113548886',
+    appId: '1:554113548886:web:ee1b75d392ef3636d4f1f7',
+    measurementId: 'G-SR7V6CNQ7Q'
+  }
 };
 
 /*
