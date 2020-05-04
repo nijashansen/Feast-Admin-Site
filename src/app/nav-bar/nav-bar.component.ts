371a7516cc4b361be5096c3175796a75f0d4927b
thor1824 <42994659+thor1824@users.noreply.github.com>
2020-05-04 13:02:14 +0200

    nav bar

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthenticationService} from '../services/authentication.service';
+import {Observable} from 'rxjs';
+import {AuthUser} from '../users/shared/user';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+
+export class NavBarComponent implements OnInit {
+  authState: AuthUser;
+
+  constructor(public auth: AuthenticationService, public router: Router) { }
+
+  ngOnInit(): void {
+    this.auth.authUser$.subscribe(value => {
+      this.authState = value;
+    });
+  }
+
+}
