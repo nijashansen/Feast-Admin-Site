e73e6e0d6e9727d7f1368ad7ca206d8d7b69d242
gtafiper <onechr@gmail.com>
2020-05-08 13:53:34 +0200

    stil

---
+++ src/app/recipes/Shared/recipe-card/recipe-card.component.ts
@@ -0,0 +1,137 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Select, Store} from '@ngxs/store';
+import {RecipesState} from '../recipes.state';
+import {Observable} from 'rxjs';
+import {Recipe} from '../recipe';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {DeleteRecipe, GetAllRecipes, UpdateRecipe} from '../recipe.action';
+
+
+@Component({
+  selector: 'app-recipe-card',
+  templateUrl: './recipe-card.component.html',
+  styleUrls: ['./recipe-card.component.css']
+})
+export class RecipeCardComponent implements OnInit {
+
+  @Input() recipe: Recipe;
+  active: boolean;
+  updateRecipe: FormGroup;
+  public edit: boolean;
+
+
+  constructor(private router: Router,
+              private formBuilder: FormBuilder,
+              private store: Store,
+              private snackBar: MatSnackBar) {
+    this.active = false;
+    this.edit = false;
+  }
+
+
+  ngOnInit() {
+
+    this.updateRecipe = new FormGroup({
+      id: new FormControl(this.recipe.id, [Validators.required]),
+      name: new FormControl(this.recipe.name, [Validators.required]),
+      estimatedTime: new FormControl(this.recipe.estimatedTime, [Validators.required, Validators.min(1)]),
+      ingredients: new FormArray([])
+    });
+    const formArray = new FormArray([]);
+    for (const ing of this.recipe.ingredients) {
+      const ingredient = new FormGroup({
+        name: new FormControl(ing.name, [Validators.required]),
+        amount: new FormControl(ing.amount, [Validators.required, Validators.min(1)])
+      });
+      formArray.push(ingredient);
+    }
+    this.updateRecipe.setControl('ingredients', formArray);
+  }
+
+
+  public switchActivation() {
+    this.active = !this.active;
+  }
+
+  onEdit() {
+    this.edit = true;
+  }
+
+  addIngredient() {
+
+    const ingredient = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      amount: new FormControl('', [Validators.required, Validators.min(1)])
+    });
+    this.ingredients.push(ingredient);
+  }
+
+
+  submitHandler() {
+    if (this.updateRecipe.valid) {
+      const updated = this.updateRecipe.value;
+      this.store.dispatch(new UpdateRecipe(updated)).toPromise()
+        .then(() => {
+          this.snackBar.open('Recipe was Updated', '',
+            {
+              duration: 6000,
+              panelClass: ['success']
+            });
+          this.edit = false;
+        })
+        .catch(reason => {
+          this.snackBar.open(reason, '',
+            {
+              duration: 6000,
+              panelClass: ['fail']
+            });
+        });
+    }
+  }
+
+  deleteIngredient(i) {
+    this.ingredients.removeAt(i);
+  }
+
+
+  deleteRecipe(recipe: Recipe) {
+    this.store.dispatch(new DeleteRecipe(recipe)).toPromise().then(() => this.snackBar.open
+    ('success', '', {duration: 600, panelClass: ['success']}))
+      .catch(reason => {
+        this.snackBar.open(reason, 'ok', {duration: 6000, panelClass: ['fail']});
+
+      });
+    this.edit = false;
+  }
+
+  public onCancel() {
+    this.edit = false;
+    this.updateRecipe.patchValue({id: this.recipe.id});
+    this.updateRecipe.patchValue({name: this.recipe.name});
+    this.updateRecipe.patchValue({estimatedTime: this.recipe.estimatedTime});
+    const formArray = new FormArray([]);
+    for (const ing of this.recipe.ingredients) {
+      const ingredient = new FormGroup({
+        name: new FormControl('', [Validators.required]),
+        amount: new FormControl('', [Validators.required, Validators.min(1)])
+      });
+      formArray.push(ingredient);
+    }
+    this.updateRecipe.setControl('ingredients', formArray);
+  }
+
+  get ingredients() {
+    return this.updateRecipe.get('ingredients') as FormArray;
+  }
+
+  get estimatedTime() {
+    return this.updateRecipe.get('estimatedTime') as FormControl;
+  }
+
+  get name() {
+    return this.updateRecipe.get('name') as FormControl;
+  }
+
+}
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -2,11 +2,12 @@ import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {Recipe} from './Shared/recipe';
 import {Router} from '@angular/router';
-import {FormArray, FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import {Select, Store} from '@ngxs/store';
-import {DeleteRecipe, GetAllRecipes, UpdateRecipe} from './Shared/recipe.action';
 import {RecipesState} from './Shared/recipes.state';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import {GetAllRecipes} from './Shared/recipe.action';
+
+
+
 
 
 @Component({
@@ -17,100 +18,28 @@ import {MatSnackBar} from '@angular/material/snack-bar';
 
 
 export class RecipesComponent implements OnInit {
-
   @Select(RecipesState.recipes)
   recipes$: Observable<Recipe[]>;
 
-  updateRecipe = new FormGroup({
-    id: new FormControl(''),
-    name: new FormControl(''),
-    estimatedTime: new FormControl(''),
-    ingredients: new FormArray([]),
-  });
-  editState = false;
-  recipeToEdit: Recipe;
+  isAddComponentActive: boolean;
 
-  constructor(private router: Router,
-              private formBuilder: FormBuilder,
-              private store: Store,
-              private snackBar: MatSnackBar) {
+  constructor(public router: Router, private store: Store) {
+    this.isAddComponentActive = false;
   }
 
-  get ingredients() {
-    return this.updateRecipe.get('ingredients') as FormArray;
-  }
-
-  ngOnInit() {
-
+  ngOnInit(): void {
     this.store.dispatch(new GetAllRecipes());
+}
 
-    this.updateRecipe = this.formBuilder.group({
-      id: '',
-      name: '',
-      estimatedTime: 0,
-      ingredients: this.formBuilder.array([])
-    });
-  }
 
-  editItem(event: Event, recipe: Recipe) {
-    console.log(recipe);
-    this.editState = true;
-    this.recipeToEdit = recipe;
-    this.updateRecipe.patchValue({id: recipe.id});
-    this.updateRecipe.patchValue({name: recipe.name});
-    this.updateRecipe.patchValue({estimatedTime: recipe.estimatedTime});
-    this.updateRecipe.value.ingredients.fill(recipe.ingredients);
+
+  public createNew() {
+    this.isAddComponentActive = true;
 
   }
 
-  clearState() {
-    this.editState = false;
-    this.recipeToEdit = null;
-  }
-
-  addIngredient() {
-    const ingredient = this.formBuilder.group({
-      name: '',
-      amount: '',
-    });
-    this.ingredients.push(ingredient);
-  }
-
-
-  submitHandler() {
-    const info = {
-      id: this.recipeToEdit.id,
-      name: this.updateRecipe.value.name,
-      estimatedTime: this.updateRecipe.value.estimatedTime,
-      ingredients: this.updateRecipe.value.ingredients
-    } as Recipe;
-    console.log(info);
-    this.store.dispatch(new UpdateRecipe(info)).toPromise()
-      .then(() => this.snackBar.open
-      ('Recipe Was Updated', '', {duration: 600, panelClass: ['success']}))
-      .catch(reason => {
-        this.snackBar.open(reason, 'ok', {duration: 6000, panelClass: ['fail']});
-
-      });
-    this.clearState();
-  }
-
-  deleteIngredient(i) {
-    this.ingredients.removeAt(i);
-  }
-
-
-  deleteRecipe(recipe: Recipe) {
-    this.store.dispatch(new DeleteRecipe(recipe)).toPromise().then(() => this.snackBar.open
-    ('success', '', {duration: 600, panelClass: ['success']}))
-      .catch(reason => {
-        this.snackBar.open(reason, 'ok', {duration: 6000, panelClass: ['fail']});
-
-      });
-    this.editState = false;
-  }
-
-  goToHome() {
-    this.router.navigate(['/home']);
+  public onCancel() {
+    this.isAddComponentActive = false;
   }
 }
+
